e0ca1f9549926456d314328286175b85b61c64c6
RaskoProgrammer <rakgopempho@gmail.com>
2026-01-19 16:18:08 +0200

    creating Databaseand tables  with sequelize and mysql

---
+++ server/models/postechat.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+    const Posts = sequelize.define("Posts", {
+        title : {
+        type: DataTypes.STRING,
+        allowNull: false,
+        },
+        postText : {
+        type: DataTypes.STRING,
+        allowNull: false,
+        },
+        username : {
+        type: DataTypes.STRING,
+        allowNull: false,
+        },
+    });
+    return Posts;
+}
