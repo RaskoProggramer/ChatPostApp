d4c5c50f5569f5cd669b594f6f0006fda23d0c87
RaskoProgrammer <rakgopempho@gmail.com>
2026-01-21 12:43:58 +0200

    connecting react and nodejs to show get request posts feed

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const db = require('./models');
+const cors = require('cors')
 
 app.use(express.json());
+app.use(cors());
 
 //Routers
 const postRoutes = require('./routes/postRoute');
@@ -10,7 +12,7 @@ app.use("/posts", postRoutes);
 
 db.sequelize.sync().then( () => {
     app.listen(3001, () =>{
-        console.log('server running on port http://localhost/3001/')
+        console.log('server running on port http://localhost:3001')
     });
 });
 
